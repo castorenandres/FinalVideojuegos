d8ac637c112802dd7fcfab50e1e9711e3994ed54
Andres Orozco <andresorocast.aoc@gmail.com>
2019-12-05 21:46:31 -0600

    boss class

--- src/Boss.ts
+++ src/Boss.ts
@@ -5,13 +5,11 @@ type coords = [number, number];
 
 class Boss {
     private position: coords = [0,0];
-    private bossWidth: number = 57;
-    private bossHeight: number = 80;
-    private sWidth = 57; // sprite width
-    private sHeight = 80; // sprite height
-    private offsetx: number = 62.1; // Sprite offset
-    private frameCounter = 0;
-    private currentCharFrame = 0;
+    private bossWidth: number = 57; // pendiente
+    private bossHeight: number = 80; // pendiente
+    private sWidth = 57; // sprite width // pendiente
+    private sHeight = 80; // sprite height // pendiente
+    private offsetx: number = 62.1; // Sprite offset // pendiente
     private click: boolean = false; // flag for mouse click
 
     // hitbox
@@ -20,7 +18,7 @@ class Boss {
     private TopSide = this.position[1];
     private BottomSide = this.position[1] + this.bossHeight;
 
-    public mouseDownHandler = (event: MouseEvent) => {
+    public mouseDownHandler = (event: MouseEvent) => { // checks if the player click the mouse
         if (event.type === "mousedown") {
             this.click = true;
         } else if (event.type === "mouseup") {
@@ -28,18 +26,45 @@ class Boss {
         }
     };
 
-    public mouseMovementHandler = (event: MouseEvent) => { // se puede usar esto con el boss
+    public mouseMovementHandler = (event: MouseEvent) => { // Mouse movement for the game
         let [coordx, coordy] = this.position;
 
         // Mouse has to be over the boss to hurt him
         if (event.offsetX < this.RightSide  && event.offsetX > this.LeftSide && event.offsetY < this.BottomSide && event.offsetY > this.TopSide) {
-            //
+            if (this.click) { // If click is true the boss is hurt and changes position
+                // call function to change position
+            }
         }
 
-        if (this.click) { // If click is true...se ocupa?
-            coordx = (event.offsetX - this.bossWidth / 2);
-            coordy = (event.offsetY - this.bossHeight / 2);
-        }
+    };
+
+    public changeBossPosition = () => {}; // no se que mas se ocupe
+
+    public update = () => {
+        // no se si se ocupe
+        const {context} = GameContext;
+        const {width, height} = context.canvas;
+        // update hitbox
+        this.RightSide = this.position[0] + this.bossWidth;
+        this.LeftSide = this.position[0];
+        this.TopSide = this.position[1];
+        this.BottomSide = this.position[1] + this.bossHeight;
+
+        // update posicion y vida/counter de clicks al boss
+    };
+
+    public render = () => {
+        const {context} = GameContext;
+        let [xpos, ypos] = this.position;
+        const sy = 0;
+
+        // Character
+        context.save();
+        context.beginPath();
+        context.translate(xpos, ypos);
+        context.drawImage(this.character,this.currentCharFrame * this.offsetx, sy, this.sWidth, this.sHeight, 0, 0,this.charWidth,this.charHeight);
+        context.closePath();
+        context.restore();
     };
 };
 
